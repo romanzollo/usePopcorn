9287713823b5201e0c2a1c0d69fc156b1e9571d8
Roman Zlagodukhin <zloyandmad@yandex.ru>
2024-07-23 20:20:51 +0200

    built static layout

---
+++ src/components/App.jsx
@@ -0,0 +1,13 @@
+// import { useState } from 'react';
+
+import NavBar from './NavBar';
+import Main from './Main';
+
+export default function App() {
+    return (
+        <>
+            <NavBar />
+            <Main />
+        </>
+    );
+}
