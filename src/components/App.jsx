8f82d1ecb5f67d51ef55b4356b2344d0fb919bf0
Roman Zlagodukhin <zloyandmad@yandex.ru>
2024-07-24 23:13:28 +0200

    added handling hover events on StarRating component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -33,9 +33,7 @@ export default function App() {
                     <WatchedMovieList watched={watched} />
                 </MoviesBox>
             </Main>
-            <StarRating maxRating={5} />
             <StarRating maxRating={10} />
-            <StarRating />
         </>
     );
 }
